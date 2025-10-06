836b19fcc930d0f1b5f4d273a2e741bad9bdb519
Varelfebrianda <varelfebriandaputra@gmail.com>
2025-10-06 07:13:42 +0700

    done pak

---
+++ index.js
@@ -0,0 +1,32 @@
+let a = 1;
+let b = 2;
+
+const tambah =() => {
+    console.log(`hasil dari pertambahan nya adalah ${a+b}`);
+}
+tambah()
+
+const kurang =() => {
+    console.log (`hasil dari pengurangan nya adalah ${a-b}`);
+}
+kurang()
+
+const kali =() => {
+    console.log (`hasil dari perkalian nya adalah ${a*b}`);
+}
+kali()
+
+const bagi =() => {
+    console.log (`hasil dari pembagian nya adalah ${a/b}`);
+}
+bagi()
+
+const pangkat =() => {
+    console.log (`hasil dari perpangkatan nya adalah ${a**b}`);
+}
+pangkat()
+
+const modulus =() => {
+    console.log (`hasil dari modulus nya adalah ${a%b}`);
+}
+modulus()
